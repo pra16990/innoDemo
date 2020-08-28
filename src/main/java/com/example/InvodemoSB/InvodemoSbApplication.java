4b4975299f252343236b1a43e752364f7a3acaef
pra16990@gmail.com <pra16990@gmail.com>
2020-08-28 17:25:48 +0530

    first commit

---
+++ src/main/java/com/example/InvodemoSB/InvodemoSbApplication.java
@@ -0,0 +1,13 @@
+package com.example.InvodemoSB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvodemoSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvodemoSbApplication.class, args);
+	}
+
+}
